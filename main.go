1297527b745ff1db6939afb90ff9ec679839bc97
Kim Milan <kim.milan@wapice.com>
2017-08-08 12:06:53 +0300

    Update

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"net/http"
 	"os"
 	"os/signal"
+	"strconv"
+	"strings"
 	"syscall"
 	"time"
 
@@ -36,10 +39,27 @@ type Tram struct {
 	} `json:"VP"`
 }
 
+// Devices comment
+type Devices struct {
+	Size  int `json:"fullSize"`
+	Items []struct {
+		ID   string `json:"deviceId"`
+		Name string `json:"name"`
+		Manu string `json:"manufacturer"`
+	} `json:"items"`
+}
+
+// TramID comment
+type TramID struct {
+	ID   string
+	Name string
+}
+
+// TramIds map
+var TramIds = make(map[string]string)
+
 // REST Get function
-func getData(response interface{}) error {
-	// TKL Live API
-	url := "http://data.itsfactory.fi/journeys/api/1/vehicle-activity"
+func getData(response interface{}, url string) error {
 
 	// Create a http client with a 10 sec timeout
 	var httpClient = &http.Client{
@@ -57,22 +77,67 @@ func getData(response interface{}) error {
 	return json.NewDecoder(r.Body).Decode(response)
 }
 
+func sendToIoT(tram Tram, ID string) {
+	fmt.Println("Sending data")
+	url := strings.Join([]string{"https://milan_knp:Devbtiu202020@my.iot-ticket.com/api/v1/process/write/", ID}, "")
+	var httpClient = &http.Client{
+		Timeout: time.Second * 10,
+	}
+	jsonStr := strings.Join([]string{`[{"name":"lat", "v":"`, string(tram.VP.Lat), `"}, {"name":"lon", "v":"`, string(tram.VP.Long), `"}, {"name":"num", "v":"`, tram.VP.Line, `"}]`}, "")
+	jsonBa := []byte(jsonStr)
+
+	_, err := httpClient.Post(url, "application/json", bytes.NewBuffer(jsonBa))
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
 // Subscription message handler for MQTT
 var handler mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
 	tram := new(Tram)
 
 	json.Unmarshal(msg.Payload(), &tram)
+
 	fmt.Println(tram.VP.Line, string(tram.VP.Lat), string(tram.VP.Long), tram.VP.Vehicle)
+	if val, ok := TramIds[tram.VP.Vehicle]; ok {
+		go sendToIoT(*tram, val)
+	}
 }
 
 func main() {
 	fmt.Println("Starting...")
+
+	// Get existing devices
+	//TramIds := make(map[string]string)
+	repeat := 0
+	offset := 0
+
+	for offset <= repeat {
+
+		devicesURL := strings.Join([]string{"https://milan_knp:Devbtiu202020@my.iot-ticket.com/api/v1/devices?limit=100&offset=", strconv.Itoa(offset)}, "")
+		devices := new(Devices)
+		getData(&devices, devicesURL)
+
+		for _, device := range devices.Items {
+			if device.Manu == "HKL" {
+				if _, ok := TramIds[device.Name]; !ok {
+					TramIds[device.Name] = device.ID
+				}
+			}
+		}
+
+		repeat = devices.Size
+		offset += 100
+	}
+
 	/* // REST
 	 // Response-struct to store HTTP GET response
 	response := new(Response)
+	// TKL Live API
+	url := "http://data.itsfactory.fi/journeys/api/1/vehicle-activity"
 
 	for {
-		getData(&response)
+		getData(&response, url)
 
 		sort.Slice(response.Body, func(i, j int) bool {
 			return response.Body[i].Journey.Line < response.Body[j].Journey.Line
@@ -105,8 +170,6 @@ func main() {
 		fmt.Println(token.Error())
 		os.Exit(1)
 	}
-
-	//time.Sleep(time.Second * 20)
 
 	exit := false
 	for exit != true {
